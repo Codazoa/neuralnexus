47c0be8f4b1b2af4ad5c5e9a3a954f17ff85ecc4
codazoa <codazoa@protonmail.com>
2023-08-03 12:54:43 -0700

    Feeds can be added and removed via preferences

--- app/preferences/feeds/page.tsx
+++ app/preferences/feeds/page.tsx
@@ -1,9 +1,14 @@
-import { FeedUrlForm } from "@/components/FeedPicker";
+import { FeedDeleteForm, FeedUrlForm } from "@/components/FeedPicker";
 import { getServerSession } from 'next-auth';
 import { prisma } from '@/lib/prisma';
-import { redirect } from 'next/navigation';
-import { SignOutButton } from '@/components/buttons';
 import { authOptions } from "../../api/auth/[...nextauth]/route"
+
+interface FeedLinks {
+  id: string;
+  userId: string;
+  feed_url: string;
+}
+
 export default async function feedsPreference () {
   const session = await getServerSession(authOptions);
 
@@ -14,10 +19,24 @@ export default async function feedsPreference () {
     }
   });
 
+  const feed_list_promise = prisma.feeds.findMany({
+    where: {
+      userId: user?.id
+    }
+  });
+
+  const feed_list = await feed_list_promise;
+
   return (
-    <div>
+    <div className='bg-gray-400 p-4'>
       <h1 className='text-2xl font-bold'>Feeds</h1>
       <FeedUrlForm user={user} />
+      <div className='pt-4'>
+        <p>Your feeds</p>
+        {feed_list.map((item: FeedLinks, i: number) =>
+          <FeedDeleteForm item={item}/>
+        )}
+      </div>
     </div>
   );
 }
--- components/FeedPicker.tsx
+++ components/FeedPicker.tsx
@@ -1,6 +1,10 @@
 'use client';
 
+import { useRouter } from "next/navigation";
+
 export function FeedUrlForm({ user }: any) {
+  const router = useRouter();
+
   const addFeed = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -17,17 +21,46 @@ export function FeedUrlForm({ user }: any) {
         'Content-Type': 'application/json'
       },
     });
-    
-    await res.json();
-  }; 
+
+    const data = await res.json();
+    router.refresh();
+  };
   
   return (
     <div>
       <h2>Add a feed</h2>
       <form onSubmit={addFeed}>
-        <input type='text' name='feed url' defaultValue='https://example.com/feed.rss' />
-        <button className="px-4 rounded border bg-gray-300 hover:bg-blue-300" type="submit">Add Feed</button>
-      </form> 
+        <div className='flex'>
+          <div className='pr-2'>
+            <button className="px-4 rounded border bg-gray-300 hover:bg-blue-300" type="submit">Add Feed</button>
+          </div>
+          <input className='border rounded px-4 w-96' type='text' name='feed url' defaultValue='https://example.com/feed.rss' />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export function FeedDeleteForm({ item }: any) {
+  const router = useRouter();
+
+  const delFeed = async () => {
+    const res = await fetch(`/api/feeds?feedId=${item.id}`, {
+      method: 'DELETE'
+    })
+
+    router.refresh();
+  }
+
+
+
+  return (
+    <div className='flex py-2'>
+      <div className='pr-2'>
+        <button className="rounded px-4 bg-gray-300"
+          onClick={delFeed}>Delete</button>
+      </div>
+      <p className='bg-white rounded px-4'>{item.feed_url}</p>
     </div>
   );
 }
